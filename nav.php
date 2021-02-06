794a99c40ff2e271a322b74e6226a773d157d50d
Joshua Maxwell <joshmax00@gmail.com>
2021-02-06 14:50:52 -0600

    Added  some content

---
+++ nav.php
@@ -0,0 +1,13 @@
+<nav>
+  <ul>
+      <li class="navContainer">
+        <a href="index.html" class="navItem">Home</a>
+      </li>
+      <li class="navContainer">
+        <a href="projects.html" class="navItem">Projects</a>
+      </li>
+      <li class="navContainer">
+        <a href="about.html" class="navItem">About me</a>
+      </li>
+    </ul>
+</nav>
