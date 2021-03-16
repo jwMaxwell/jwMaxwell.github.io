f11138c05231b16018383247c49dbed85c735fc7
Joshua Maxwell <joshmax00@gmail.com>
2021-03-16 10:53:18 -0500

    Added (possibly) redirect

--- temp.php
+++ temp.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="utf-8" />
     <meta name="author" content="Joshua Maxwell">
-    <meta http-equiv="refresh" content="0; url = temp.php"/>
     <title>JWMaxwell Welcome page</title>
 
     <link rel="stylesheet" href="style.css" />
