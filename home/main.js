44d52db0dd33a14ed6d9efcc2aade99098683c0a
Joshua Maxwell <49821768+jwMaxwell@users.noreply.github.com>
2021-12-16 19:09:23 -0600

    Update main.js

--- home/main.js
+++ home/main.js
@@ -4,14 +4,16 @@
       .map(
         (p) =>
           `
-           <div class="window item">
-            <div class="title-bar">
-              <div class="title-bar-text">${p.title}</div>
-              <div class="title-bar-controls"></div>
-            </div>
-            <div class="window-body">
-              <a href="${p.filePath}"><img src="${p.filePath}/image.png"/></a>
-              <div class="desc">${p.desc}</div>
+           <dic class="item">
+            <div class="window">
+              <div class="title-bar">
+                <div class="title-bar-text">${p.title}</div>
+                <div class="title-bar-controls"></div>
+              </div>
+              <div class="window-body">
+                <a href="${p.filePath}"><img src="${p.filePath}/image.png"/></a>
+                <div class="desc">${p.desc}</div>
+              </div>
             </div>
           </div>
            `
