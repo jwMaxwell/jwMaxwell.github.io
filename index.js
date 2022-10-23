cf449102cf160c4dbc85bdf99a6a774c494ccd63
Joshua Maxwell <49821768+jwMaxwell@users.noreply.github.com>
2022-10-23 16:13:51 -0500

    Update index.js

--- index.js
+++ index.js
@@ -71,8 +71,21 @@ document.getElementById("code").addEventListener(
     const text = document.getElementById("code");
     hljs.highlightAll(text.innerText);
 
-    _.preventDefault();
+    caretToEnd(text);
     text.focus();
   },
   false
 );
+
+const caretToEnd = (element) => {
+  let range, selection;
+  if (document.createRange) {
+    //Firefox, Chrome, Opera, Safari, IE 9+
+    range = document.createRange(); //Create a range (a range is a like the selection but invisible)
+    range.selectNodeContents(element); //Select the entire contents of the element with the range
+    range.collapse(false); //collapse the range to the end point. false means collapse to end rather than the start
+    selection = window.getSelection(); //get the selection object (allows you to change selection)
+    selection.removeAllRanges(); //remove any selections already made
+    selection.addRange(range); //make the range you have just created the visible selection
+  }
+};
