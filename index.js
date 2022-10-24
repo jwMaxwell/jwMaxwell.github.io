08f31fb6fc0daf9d23801edbc155cebc6eab2d78
Joshua Maxwell <49821768+jwMaxwell@users.noreply.github.com>
2022-10-23 22:38:38 -0500

    fixed tabs

--- index.js
+++ index.js
@@ -63,43 +63,13 @@ document.getElementById("code").addEventListener(
   function (e) {
     const cursor = document.getElementById("code").selectionStart;
     if (e.keyCode === 9) {
-      this.value += "    ";
+      this.value = inject(this.value, cursor, "    ");
       e.preventDefault();
+      document.getElementById("code").selectionEnd = cursor + 4;
     }
-    document.getElementById("code").selectionStart = cursor;
   },
   false
 );
 
-// function getCursorPos(input) {
-//   if ("selectionStart" in input && document.activeElement == input) {
-//     return {
-//       start: input.selectionStart,
-//       end: input.selectionEnd,
-//     };
-//   } else if (input.createTextRange) {
-//     var sel = document.selection.createRange();
-//     if (sel.parentElement() === input) {
-//       var rng = input.createTextRange();
-//       rng.moveToBookmark(sel.getBookmark());
-//       for (
-//         var len = 0;
-//         rng.compareEndPoints("EndToStart", rng) > 0;
-//         rng.moveEnd("character", -1)
-//       ) {
-//         len++;
-//       }
-//       rng.setEndPoint("StartToStart", input.createTextRange());
-//       for (
-//         var pos = { start: 0, end: len };
-//         rng.compareEndPoints("EndToStart", rng) > 0;
-//         rng.moveEnd("character", -1)
-//       ) {
-//         pos.start++;
-//         pos.end++;
-//       }
-//       return pos;
-//     }
-//   }
-//   return -1;
-// }
+const inject = (str, pos, val) =>
+  [str.slice(0, pos), val, str.slice(pos, str.length)].join("");
