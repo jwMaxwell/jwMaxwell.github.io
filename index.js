0a342de8f059b530479605cf230ce1f301a65f8f
Joshua Maxwell <49821768+jwMaxwell@users.noreply.github.com>
2022-10-23 22:25:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -57,13 +57,49 @@ const setLang = (lang, version) => {
   options.body = JSON.stringify(code);
 };
 
+// Tab makes tabs
 document.getElementById("code").addEventListener(
   "keydown",
   function (e) {
+    const cursor = document.getElementById("code").selectionStart;
     if (e.keyCode === 9) {
       this.value += "    ";
       e.preventDefault();
     }
+    document.getElementById("code").selectionStart = cursor;
   },
   false
 );
+
+// function getCursorPos(input) {
+//   if ("selectionStart" in input && document.activeElement == input) {
+//     return {
+//       start: input.selectionStart,
+//       end: input.selectionEnd,
+//     };
+//   } else if (input.createTextRange) {
+//     var sel = document.selection.createRange();
+//     if (sel.parentElement() === input) {
+//       var rng = input.createTextRange();
+//       rng.moveToBookmark(sel.getBookmark());
+//       for (
+//         var len = 0;
+//         rng.compareEndPoints("EndToStart", rng) > 0;
+//         rng.moveEnd("character", -1)
+//       ) {
+//         len++;
+//       }
+//       rng.setEndPoint("StartToStart", input.createTextRange());
+//       for (
+//         var pos = { start: 0, end: len };
+//         rng.compareEndPoints("EndToStart", rng) > 0;
+//         rng.moveEnd("character", -1)
+//       ) {
+//         pos.start++;
+//         pos.end++;
+//       }
+//       return pos;
+//     }
+//   }
+//   return -1;
+// }
