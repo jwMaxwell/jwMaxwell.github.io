340d1a9c8778a914aad1f0c7420861b62e621c2f
Joshua Maxwell <49821768+jwMaxwell@users.noreply.github.com>
2023-01-23 13:19:20 -0600

    Added default values

--- VMC/langController.js
+++ VMC/langController.js
@@ -1,6 +1,11 @@
 import { runASM } from "./asm.js";
 import { runVMC } from "./vmc.js";
 
+window.onload = () => {
+  vmc.innerText = runASM(asm.value);
+  output.innerText = runVMC(runASM(asm.value));
+};
+
 asm.addEventListener("keyup", (e) => {
   vmc.innerText = runASM(asm.value);
   output.innerText = runVMC(runASM(asm.value));
