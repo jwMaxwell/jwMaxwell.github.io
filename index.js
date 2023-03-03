12c15e882e53d303189fc5d0adaa5090adcc84ac
Joshua Maxwell <49821768+jwMaxwell@users.noreply.github.com>
2023-03-03 15:01:06 -0600

    updates

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-// <!DOCTYPE html>
-// <html>
-//   <head>
-//     <title>Joshua Maxwell</title>
-//     <link rel="stylesheet" href="./style.css" />
-//   </head>
-//   <body>
-//     <h2>Code projects:</h2>
-//     <div id="projects"></div>
-//     <a href="./code/">Code editor and execution engine</a>
-//     <br / >
-//     <a href="./VMC/">Virtual machine code + assembly + intermediary</a>
-//     <br / >
-//     <a href="./lisp/">Lisp interpreter</a>
-
-//     <h2>Code blog:</h2>
-//     <div id="blogs"></div>
-//     <p>You'll just have to wait and simmer while I prepare these</p>
-//   </body>
-// </html>
-
 const response = await fetch("./home-data.json");
 const data = await response.json();
 
