845ea9b0b95bee0d2364f22fa8c44addcaa3acce
Joshua Maxwell <49821768+jwMaxwell@users.noreply.github.com>
2023-03-02 21:35:50 -0600

    updates

--- Neural/src/test.js
+++ Neural/src/test.js
@@ -36,33 +36,12 @@ const trainingData = [
   },
 ];
 
-// const trainingData = [
-//   {
-//     input: [0, 0],
-//     output: [0],
-//   },
-//   {
-//     input: [0, 0.1],
-//     output: [1],
-//   },
-//   {
-//     input: [0, 1],
-//     output: [1],
-//   },
-//   {
-//     input: [0, 0.3],
-//     output: [1],
-//   },
-// ];
-
 const print = (x, y) =>
   console.log(`${x} -> result: ${Math.round(y)}, actual: ${y}`);
 const run = () => {
   let network1 = new Network([3, 15, 15, 1]).train(trainingData, 200000);
   for (const n of trainingData) print(n.input, network1.run(n.input));
-  console.log();
-  let network2 = new Network().from(network1.toJSON());
-  for (const n of trainingData) print(n.input, network2.run(n.input));
+  console.log(JSON.parse(JSON.stringify(network1.toJSON())));
 };
 
 run();
