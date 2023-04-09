8d0cca709a04ff50067b9ad760a22c370ca2fa2a
Joshua Maxwell <49821768+jwMaxwell@users.noreply.github.com>
2023-04-08 22:38:31 -0500

    Update url-crunch.js

--- code/src/url-crunch.js
+++ code/src/url-crunch.js
@@ -1,50 +1,68 @@
-const appendChar = (charCode) =>
-  charCode < 32 || charCode == 127
-    ? `\`${String.fromCharCode((charCode + 34) & 127)}`
-    : charCode == 96
-    ? `\`${String.fromCharCode(charCode)}`
-    : String.fromCharCode(charCode);
-
-const appendChunk = (numMatchingChars, charCode, numLastMatchChars) =>
-  "`" +
-  String.fromCharCode(numMatchingChars) +
-  String.fromCharCode(charCode + 33) +
-  String.fromCharCode(numLastMatchChars + 33);
+const appendChar = (charCode) => {
+  let res = "";
+  if (charCode < 32 || charCode == 127) {
+    res += "`";
+    charCode = (charCode + 34) & 127;
+  } else if (charCode == 96) {
+    res += "`";
+  }
+  res += String.fromCharCode(charCode);
+  return res;
+};
 
 export const compress = (str) => {
   let result = "";
+  let index = 0;
   let chunkSize = 0;
-  let lastMatchChars = 0;
-  let matchingChars = 0;
+  let numLastMatchChars = 0;
+  let numMatchingChars = 0;
+  let charCode;
 
-  for (let index = 0; index < str.length; ) {
-    chunkSize = Math.max(5, chunkSize);
-    if (!(index + chunkSize > str.length)) {
-      let searchStart = index > 8840 ? index - 8840 : 0;
-      let matchIndex = str
-        .slice(searchStart, index)
-        .lastIndexOf(str.slice(index, index + chunkSize));
-      if (matchIndex >= 0) {
-        lastMatchChars = index - (searchStart + matchIndex);
-        matchingChars = chunkSize++;
-        continue;
-      } else if (!matchingChars) {
-        result += appendChar(str.charCodeAt(index++));
-        continue;
+  const appendChunk = () => {
+    if (numMatchingChars) {
+      index += numMatchingChars;
+      chunkSize -= numMatchingChars;
+      result += "`";
+      numLastMatchChars -= 5;
+      numMatchingChars -= 5;
+      numMatchingChars += 66;
+      if (numMatchingChars >= 96) {
+        numMatchingChars += 1;
       }
+      result += String.fromCharCode(numMatchingChars);
+      charCode = numLastMatchChars % 94;
+      numLastMatchChars = (numLastMatchChars - charCode) / 94;
+      result +=
+        String.fromCharCode(charCode + 33) +
+        String.fromCharCode(numLastMatchChars + 33);
+      numMatchingChars = 0;
     }
-    if (matchingChars) {
-      index += matchingChars;
-      chunkSize -= matchingChars;
-      matchingChars += matchingChars >= 35 ? 62 : 61;
-      let charCode = (lastMatchChars - 5) % 94;
-      lastMatchChars = (lastMatchChars - charCode) / 94;
-      result += appendChunk(matchingChars, charCode, lastMatchChars);
-      matchingChars = 0;
-    }
+  };
 
+  while (index < str.length) {
+    chunkSize = Math.max(5, chunkSize);
     if (index + chunkSize > str.length) {
-      while (index < str.length) result += appendChar(str.charCodeAt(index++));
+      appendChunk();
+      let numMatchedChars = str.length - index;
+      result += appendChar(str.charCodeAt(index++));
+      while (--numMatchedChars) {
+        result += appendChar(str.charCodeAt(index++));
+      }
+    } else {
+      let chunk = str.substr(index, chunkSize);
+      let searchStart = index > 8840 ? index - 8840 : 0;
+      let matchIndex = str.substring(searchStart, index).lastIndexOf(chunk);
+      if (matchIndex >= 0) {
+        numLastMatchChars = index - (searchStart + matchIndex);
+        numMatchingChars = chunkSize++;
+        if (numMatchingChars >= 64) {
+          appendChunk();
+        }
+      } else if (numMatchingChars) {
+        appendChunk();
+      } else {
+        result += appendChar(str.charCodeAt(index++));
+      }
     }
   }
 
@@ -57,20 +75,25 @@ export const decompress = (compressed) => {
 
   while (index < compressed.length) {
     let currChar = compressed.charCodeAt(index++);
+
     if (currChar === 96) {
       currChar = compressed.charCodeAt(index++);
-      if (currChar > 65) {
+      if (currChar === 96) {
+        output += String.fromCharCode(currChar);
+      } else if (currChar > 65) {
         currChar -= currChar > 96 ? 62 : 61;
         const length = currChar;
         currChar = compressed.charCodeAt(index++);
         let offset = currChar - 28;
         currChar = compressed.charCodeAt(index++);
         offset += 94 * (currChar - 33);
-        const segStart = output.length - offset;
-        output += output.slice(segStart, segStart + length);
-      } else if (currChar > 32)
+        output += output.substr(output.length - offset, length);
+      } else if (currChar > 32) {
         output += String.fromCharCode((currChar - 34) & 127);
-    } else output += String.fromCharCode(currChar);
+      }
+    } else {
+      output += String.fromCharCode(currChar);
+    }
   }
 
   return output;
