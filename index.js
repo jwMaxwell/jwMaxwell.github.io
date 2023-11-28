96efc50abe32f491507ba2cacacf70b45ee8505a
Joshua Maxwell <49821768+jwMaxwell@users.noreply.github.com>
2023-11-28 13:21:20 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-const response = await fetch('./home-data.json');
-const {projects, blogs} = await response.json();
+const response = await fetch("./home-data.json");
+const { projects, blogs } = await response.json();
 
-const projectSection = ({path, title, description}) => `
+const projectSection = ({ path, title, description }) => `
   <section>
     <a href="${path}">${title}</a> <br />
     <p>${description}</p>
   </section>`;
 
-const blogSection = ({path, title}) => `<a href="${path}">${title}</a> <br />`;
+const blogSection = ({ path, title }) =>
+  `<a href="${path}">${title}</a> <br />`;
 
 document.body.innerHTML += `
-<h2>Code projects:</h2>
-<div id="projects">${projects.map(projectSection).join('')}</div>
+<h2 class="content">Code projects:</h2>
+<div id="projects" class="content">${projects
+  .map(projectSection)
+  .join("")}</div>
 
-<h2>Code blog:</h2>
-<div id="blogs">${blogs.map(blogSection).join('')}</div>`;
+<h2 class="content">Code blog:</h2>
+<div id="blogs" class="content">${blogs.map(blogSection).join("")}</div>`;
