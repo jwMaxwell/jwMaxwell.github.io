8991d857c7fef61f3340e56e588128bb60fbf67e
Joshua Maxwell <49821768+jwMaxwell@users.noreply.github.com>
2023-11-28 20:59:42 -0600

    Update webgl.js

--- webgl.js
+++ webgl.js
@@ -15,44 +15,55 @@ const fragWrap = (str) =>
     gl_FragColor = fragColor;
 }`;
 
-document.addEventListener("DOMContentLoaded", async () => {
-  const response = await fetch("./shader.frag");
-  const fragmentShader = fragWrap(await response.text());
+const response = await fetch("./shader.frag");
+const fragmentShader = fragWrap(await response.text());
 
-  const renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-  const material = new THREE.ShaderMaterial({
-    fragmentShader: fragmentShader,
-    uniforms: {
-      iResolution: {
-        value: new THREE.Vector3(window.innerWidth, window.innerHeight, 1),
-      },
-      iTime: { value: 0.0 },
+const material = new THREE.ShaderMaterial({
+  fragmentShader: fragmentShader,
+  uniforms: {
+    iResolution: {
+      value: new THREE.Vector3(window.innerWidth, window.innerHeight, 1),
     },
-  });
-
-  const canvas = document.querySelector("canvas");
-  const camera = new THREE.PerspectiveCamera(
-    10,
-    canvas.width / canvas.height,
-    0.1,
-    1000
-  );
-  camera.position.z = 5;
-
-  const geometry = new THREE.PlaneGeometry(2, 2);
-  const plane = new THREE.Mesh(geometry, material);
-
-  const scene = new THREE.Scene();
-  scene.add(plane);
-
-  const render = () => {
-    requestAnimationFrame(render);
-    material.uniforms.iTime.value += 0.01;
-    renderer.render(scene, camera);
-  };
-
-  render();
+    iTime: { value: 0.0 },
+  },
 });
+
+const canvas = document.querySelector("canvas");
+const camera = new THREE.PerspectiveCamera(
+  10,
+  canvas.width / canvas.height,
+  0.1,
+  1000
+);
+camera.position.z = 5;
+
+const geometry = new THREE.PlaneGeometry(2, 2);
+const plane = new THREE.Mesh(geometry, material);
+
+const scene = new THREE.Scene();
+scene.add(plane);
+
+const render = () => {
+  requestAnimationFrame(render);
+  material.uniforms.iTime.value += 0.01;
+  renderer.render(scene, camera);
+};
+
+addEventListener(
+  "resize",
+  () => {
+    material.uniforms.iResolution.value.set(
+      window.innerWidth,
+      window.innerHeight,
+      1
+    );
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  },
+  false
+);
+
+render();
